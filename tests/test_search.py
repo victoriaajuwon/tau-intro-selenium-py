8269c011eac5c56bc3bf4b613a2c872c5e879288
victoriaajuwon <oluwapelumi.ajuwon@gmail.com>
2024-03-21 15:32:27 -0600

    Completed code with defining page objects, finding locators for element, making webdriver calls, configuring multiple calls, handling race conditions, running test in parallel

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,23 +1,40 @@
 """
 These tests cover DuckDuckGo searches.
 """
+import pytest
+
+from pages.result import DuckDuckGoResultPage
+from pages.search import DuckDuckGoSearchPage
 
 
-def test_basic_duckduckgo_search():
+@pytest.mark.parametrize('phrase', ['panda', 'python', 'polar bear'])
+def test_basic_duckduckgo_search(browser, phrase):
+    search_page = DuckDuckGoSearchPage(browser)
+    result_page = DuckDuckGoResultPage(browser)
+
     # Given the DuckDuckGo home page is displayed
-    # TODO
+    search_page.load()
 
-    # When the user searches for "panda"
-    # TODO
+    # When the user searches for phrase
+    search_page.search(phrase)
 
     # Then the search result title contains "panda"
-    # TODO
+    # assert PHRASE in result_page.title()
+    # from selenium.webdriver.support.ui import WebDriverWait
+    # from selenium.webdriver.support import expected_conditions as EC
+    # WebDriverWait(driver, 10).until(EC.title_contains(PHRASE))
 
-    # And the search result query is "panda"
-    # TODO
+    # Then the search result query is phrase
+    assert phrase == result_page.search_input_values()
 
-    # And the search result links pertain to "panda"
-    # TODO
+    # And the search result links pertain to phrase
+    # for title in result_page.result_link_titles():
+    #     assert PHRASE.lower() in title.lower()
 
-    raise Exception("Incomplete Test")
+    titles = result_page.result_link_titles()
+    matches = [t for t in titles if phrase.lower() in t.lower()]
+    assert len(matches) > 0
 
+    # And the search result title contains phrase
+    # (Putting this assertion last guarantees that the page title will be ready)
+    assert phrase in result_page.title()
